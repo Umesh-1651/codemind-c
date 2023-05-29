0352b681fabc1aefb1fb33b6e532ed5221598f44
Umesh Hemanth Shivaji <122421297+Uma-1651@users.noreply.github.com>
2023-05-29 09:03:25 +0530

    #include <stdio.h> int main() {     int r;     scanf("%d",&r);     for(int i=1;i<=r;i++){         for(int j=1;j<=i;j++){             printf("%d",j);         }         printf(" ");     } }

---
+++ Pattern_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int r;
+    scanf("%d",&r);
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",j);
+        }
+        printf("
+");
+    }
+}
