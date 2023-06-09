5f96d84c99023a2ae8d9cc537c5a2b63aec9af5d
Umesh Hemanth Shivaji <122421297+Uma-1651@users.noreply.github.com>
2023-06-09 21:10:18 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n];     int sum=0;     for(int i=0;i<n;i++){         scanf("%d",&a[i]);         if(a[i] ==0 || a[i] ==  1){             sum+=1;         }     }     if(sum == n) printf("True");     else printf("False");
    }

---
+++ Binary_Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        if(a[i] ==0 || a[i] ==  1){
+            sum+=1;
+        }
+    }
+    if(sum == n) printf("True");
+    else printf("False");
+
+}
