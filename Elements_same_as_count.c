930003c85cdda79123bc1c2dd44ca1d0a3352616
Umesh Hemanth Shivaji <122421297+Uma-1651@users.noreply.github.com>
2023-09-10 19:55:40 +0530

    #include<stdio.h>
    #define MAX_N 100
    
    int main()
    {
      int n, arr[MAX_N];
      int skip[MAX_N]; // this is used for skipping already visited numbers
    
      int sum = 0;
      int items = 0;
      double res;
      scanf("%d",&n);
      if ( n > MAX_N)
      {
        printf("invalid argument");
        return -1;
      }
    
      for(int i =0 ;i < n; ++i)
      {
        scanf("%d",&arr[i]);
        skip[i] = 0;
      }
    
      for(int i=0; i<n; i++)
      {
        int count = 0;
    
        if (skip[i]) continue;
    
        for(int j=0; j<n; j++)
        {
          if(arr[i]==arr[j])
          {
            ++count;
            skip[j] = 1;
          }
        }
        if(arr[i] == count)
        {
          items++;
          printf("%d ",arr[i]);
        }
      }
    
      if (items == 0)
      {
        printf("-1");
      }
      return 0;
    }

---
+++ Elements_same_as_count.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+#define MAX_N 100
+
+int main()
+{
+  int n, arr[MAX_N];
+  int skip[MAX_N]; // this is used for skipping already visited numbers
+
+  int sum = 0;
+  int items = 0;
+  double res;
+  scanf("%d",&n);
+  if ( n > MAX_N)
+  {
+    printf("invalid argument");
+    return -1;
+  }
+
+  for(int i =0 ;i < n; ++i)
+  {
+    scanf("%d",&arr[i]);
+    skip[i] = 0;
+  }
+
+  for(int i=0; i<n; i++)
+  {
+    int count = 0;
+
+    if (skip[i]) continue;
+
+    for(int j=0; j<n; j++)
+    {
+      if(arr[i]==arr[j])
+      {
+        ++count;
+        skip[j] = 1;
+      }
+    }
+    if(arr[i] == count)
+    {
+      items++;
+      printf("%d ",arr[i]);
+    }
+  }
+
+  if (items == 0)
+  {
+    printf("-1");
+  }
+  return 0;
+}
